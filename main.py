7d9555f07d9316c76b949f29ba1733f567b9b09e
Mateusz Kocot <mateuszkocot99@gmail.com>
2021-04-24 13:52:21 +0200

    world simulation

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
-from numpy import random
- 
-seed = sum([ord(c) for c in 'And I will strike down upon thee with great vengeance and furious anger'])
-np.random.seed(seed)
+import random
+
+seed = sum([ord(c) for c in 'Tuturururuuttuutut'])
+# np.random.seed(seed)
 
 ##### model config #####
 # Weapon number
-W = 6
+W = 60
 # Target number
-T = 4
+T = 40
 # Target values
 V = np.random.uniform(low=1, high=10, size=(T))
 # Probability of target's destruction
@@ -16,17 +16,96 @@ K = np.random.uniform(low=0, high=1, size=(W, T))
 
 ##### GA config #####
 # number of chromosomes
-start_chromosomes = 20 * 2 + 1 * 3 + 2 * 2
+start_chromosomes = 80
 # population
-P = np.random.randint(low=0, high=T, size=(start_chromosomes, W))
+# P = np.random.randint(low=0, high=T, size=(start_chromosomes, W))
 
 def survival_fun(X, V, K):
     return np.sum(V * np.prod(np.power(1 - K, X), axis=0))
     
 def chromosome_to_X(chromosome, T):
+    # print(chromosome)
     return np.eye(T)[chromosome]
 
-print(chromosome_to_X(P[0], T))
-print(np.power(1 - K, chromosome_to_X(P[0], T)))
-print(np.prod( np.power(1 - K, chromosome_to_X(P[0], T)), axis=0))
-print(survival_fun(chromosome_to_X(P[0], T), V, K))
+# print(P[0])
+# print(chromosome_to_X(P[0], T))
+# print(np.power(1 - K, chromosome_to_X(P[0], T)))
+# print(np.prod( np.power(1 - K, chromosome_to_X(P[0], T)), axis=0))
+# print(survival_fun(chromosome_to_X(P[0], T), V, K))
+
+# na kolejne zajęcia - krzyżowanie
+
+
+good_genes = np.argmax(K * V, axis=1)
+
+A = np.array([1, 1, 3, 3, 2, 0])
+B = np.array([2, 1, 2, 2, 3, 2])
+
+
+def EX(A, B, mc):   # crossover
+    A, B = A.copy(), B.copy()
+    for _ in range(mc):
+        inherited_good_genes = np.logical_and(A == B, A == good_genes)  # find good genes that are the same in both parents
+        bad_genes = np.where(inherited_good_genes==False)[0]
+        if len(bad_genes) <= 2 :
+            break
+        gene1, gene2 = np.random.choice(bad_genes, 2, replace=False)
+        A[gene1], A[gene2], B[gene1], B[gene2] = B[gene2], B[gene1], A[gene2], A[gene1]
+    return A, B
+
+def mutate(A, mm):
+    indices_to_mutate = np.random.choice(np.arange(0, W), mm, replace=False)
+    for idx in indices_to_mutate:
+        A[idx] = np.random.randint(0, T)
+
+def select(P, P_values, D):
+    D_values = np.array([survival_fun(chromosome_to_X(a, T), V, K) for a in D])
+    # all_values, all_chromosomes = np.concatenate((P_values, D_values)), np.concatenate((P, D))
+    all_values = np.concatenate((P_values, D_values))
+    all_chromosomes = np.concatenate((P, D))
+    indices = np.argpartition(all_values, start_chromosomes)[:start_chromosomes]
+    new_P = np.take(all_chromosomes, indices, axis=0)
+    new_values = np.take(all_values, indices)
+    return new_P, new_values
+    
+def generate_offspring(P):
+    # D=np.empty(shape=(start_chromosomes, W))
+    D = []
+    for i in range(0, start_chromosomes, 2):  
+        A, B = EX(P[i], P[i+1], 3)  #TODO MAKE RANDOM
+        D.append(A)
+        D.append(B)
+        mutate(D[i], 2) # TODO MAKE RANDOM
+        mutate(D[i+1], 2) # TODO MAKE RANDOM
+    D = np.array(D)
+    return D
+
+def next_generation(P, P_values):
+    D = generate_offspring(P)
+    return select(P, P_values, D)
+
+def simulate_world(turns):
+    P = np.random.randint(low=0, high=T, size=(start_chromosomes, W))
+    P_values = np.array([survival_fun(chromosome_to_X(a, T), V, K) for a in P])
+    idx = np.argmin(P_values)
+    print(P_values[idx], P[idx])
+    for _ in range(turns):
+        P, P_values = next_generation(P, P_values)
+        
+    idx = np.argmin(P_values)
+    return P_values[idx], P[idx]
+
+p_val_min, p_min = simulate_world(10000)
+print(p_val_min, p_min)
+# print(EX(A,B, 5))
+# a, b = EX(A,B, 5)
+# print(survival_fun(chromosome_to_X(A, T), V, K))
+# print(survival_fun(chromosome_to_X(B, T), V, K))
+# print(survival_fun(chromosome_to_X(a, T), V, K))
+# print(survival_fun(chromosome_to_X(b, T), V, K))
+# mutate(a, 2)
+# print(survival_fun(chromosome_to_X(a, T), V, K))
+
+
+# print(select([A,B], [survival_fun(chromosome_to_X(A, T), V, K), survival_fun(chromosome_to_X(B, T), V, K)], [a, b]))
+
